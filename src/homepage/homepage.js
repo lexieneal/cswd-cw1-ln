d6f786ce6b5878a3b7a7fcfded23c276ffe68593
lexieneal <lexieneal13@gmail.com>
2022-08-07 17:48:40 +0100

    trying to commit images to see if it helps with carousel

--- src/homepage/homepage.js
+++ src/homepage/homepage.js
@@ -1,15 +1,34 @@
-import React, { Component } from 'react';
-import '../app/app.css';
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.css';
 
-class Homepage extends Component {
-  render() {
-    return (
-       <div class="home">
-        <p>Home page</p>
-        <p>Add more information here</p>
-      </div>
-    )
-  }
-};
+function Homepage() {
+  return (
+    <div style={{ display: 'block', width: 700, padding: 30 }}>
+      <h4>React-Bootstrap Carousel Component</h4>
+      <Carousel>
+
+        <Carousel.Item interval={1500}>
+          <img className="d-block w-100" src="../images/backpacking1.jpg" alt="Image One"/>
+          <Carousel.Caption>
+            <h3>Label for first slide</h3>
+            <p>Sample Text for Image One</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item interval={500}>
+          <img className="d-block w-100" src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png" alt="Image Two"/>
+          <Carousel.Caption>
+            <h3>Label for second slide</h3>
+            <p>Sample Text for Image Two</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+      </Carousel>
+    </div>
+  );
+}
+
+
 
 export default Homepage;
